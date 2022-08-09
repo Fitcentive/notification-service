93d3eea959e978bb48afd54373205e5d35900056
Vidhyasagar Harihara <vidhyasagar1210@gmail.com>
2022-08-08 23:36:19 -0400

    - Fix evolutions script

--- conf/evolutions/default/3.sql
+++ conf/evolutions/default/3.sql
@@ -1,7 +1,6 @@
 # -- !Ups
 
-insert into notification_types (name, description)
-values ('UserCommentedOnPost', 'Notification type when user A has their social media post was commented on by user B'),
-values ('UserLikedPost', 'Notification type when user A has their social media post was liked by user B');
+insert into notification_types (name, description) values ('UserCommentedOnPost', 'Notification type when user A has their social media post was commented on by user B');
+insert into notification_types (name, description) values ('UserLikedPost', 'Notification type when user A has their social media post was liked by user B');
 
 # -- !Downs
