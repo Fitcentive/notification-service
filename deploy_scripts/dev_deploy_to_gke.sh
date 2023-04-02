e3bfd8aad9b656666efdac60bf3b9d30b0a95d06
Vidhyasagar Harihara <vidhyasagar1210@gmail.com>
2023-04-02 18:34:25 -0400

    feat: Change deployment to fitcentive-dev-02 project

--- deploy_scripts/dev_deploy_to_gke.sh
+++ deploy_scripts/dev_deploy_to_gke.sh
@@ -3,7 +3,7 @@
 kubectl delete -n notification deployment/notification-service
 
 # Delete old 1.0 image from gcr
-echo "y" | gcloud container images delete gcr.io/fitcentive-dev/notification:1.0 --force-delete-tags
+echo "y" | gcloud container images delete gcr.io/fitcentive-dev-02/notification:1.0 --force-delete-tags
 
 # Build and push image to gcr
 sbt docker:publish
